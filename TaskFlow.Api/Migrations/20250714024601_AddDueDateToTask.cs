f3ef6bf238151e3b1bcd79821fb60d5f693b4132
Agustin Conde <conde.agustin66@gmail.com>
2025-07-14 00:02:41 -0300

    feat: add migration for DueDate field

---
+++ TaskFlow.Api/Migrations/20250714024601_AddDueDateToTask.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskFlow.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDueDateToTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "Tasks",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}

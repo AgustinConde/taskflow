def1385f985999e800b3cd611894ada446cc0edf
Agustin Conde <conde.agustin66@gmail.com>
2025-08-26 03:10:36 -0300

    feat: export NotificationContext from context file
    NotificationContext is now exported to allow usage in other modules.

--- taskflow-frontend/src/contexts/NotificationContext.tsx
+++ taskflow-frontend/src/contexts/NotificationContext.tsx
@@ -22,7 +22,7 @@ interface NotificationContextType {
     clearAll: () => void;
 }
 
-const NotificationContext = createContext<NotificationContextType | undefined>(undefined);
+export const NotificationContext = createContext<NotificationContextType | undefined>(undefined);
 
 export const useNotifications = () => {
     const context = useContext(NotificationContext);
